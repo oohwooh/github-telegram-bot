30b3ab698b93582e017b6f354711e09419b5d9ad
Elizabeth Qiu <elizabethjqiu@gmail.com>
2020-07-07 18:29:12 -0400

    import os, add method call to updater, add basic greeting to make bot working, add BOT_TOKEN file to .gitignore

--- bot.py
+++ bot.py
@@ -2,9 +2,19 @@
 
 #some packages are built-in but for others you need to import // others req installing pip (pip install python-telegram-bot)
 from telegram.ext import Updater
-updater = Updater(token='TOKEN', use_context=True) #use_context=True is special for v12 of library; default=False
+import os
+updater = Updater(token=os.getenv("BOT_TOKEN"), use_context=True) #use_context=True is special for v12 of library; default=False
 dispatcher = updater.dispatcher #introduce locally for updater quicker access to dispatcher
 
 import logging #logging module setup to know when/why if things don't work
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
 
