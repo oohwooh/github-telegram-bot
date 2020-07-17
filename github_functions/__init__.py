f49edf9849b102793d8343b188e1cf089809cf14
Elizabeth Qiu <elizabethjqiu@gmail.com>
2020-07-17 11:54:11 -0400

    add github functions to module

--- github_functions/__init__.py
+++ github_functions/__init__.py
@@ -1,1 +1,12 @@
+#explicitly state files and functions
+#makes it a module (like a "package" but not on pip)
 from .repo_overview import repo_overview
+from .createrepo import create_repo
+from .deleterepo import delete_repo
+from .forkrepo import fork_repo
+from .invite import invite_collab
+from .listrepos import list_repos
+from .recentcommits import recent_commits
+from .searchrepos import search_repos
+from .starrepo import star_repo
+from .watch import watch
