aba0ed6643db7a82fee92612d6c22a0ea816ea9a
Kavya Somala <somala.kavya123@gmail.com>
2020-07-17 15:01:06 -0400

    Added the number of watchers, stars, and forks into the description in repo overview.

--- github_functions/repo_overview.py
+++ github_functions/repo_overview.py
@@ -4,6 +4,6 @@ from telegram import InlineKeyboardButton
 def repo_overview(repoName, token):
     g = Github(token)
     repo = g.get_repo(repoName)
-    desc = "This repo is written in " + str(repo.get_languages()) + " and the description for the repo is: " + repo.description
+    desc = "Description: " + repo.description + "\n Languages: " + str(repo.get_languages()) + "\n Watchers: " + str(repo.watchers_count) + " \n Stargazers: " + str(repo.stargazers_count) + " \n Forks: " + str(repo.forks_count)
     keyboard = [[InlineKeyboardButton("⭐", callback_data='star_'+str(repo.id)), InlineKeyboardButton("👀", callback_data='watch_'+str(repo.id))], [InlineKeyboardButton("Open in GitHub", url=repo.url)]]
     return desc, keyboard
