fcae3f3333233e39313eb747655a0dedb8abdca0
Elizabeth Qiu <elizabethjqiu@gmail.com>
2020-07-22 17:41:26 -0400

    fix watchrepo function

--- github_functions/watchrepo.py
+++ github_functions/watchrepo.py
@@ -1,9 +1,14 @@
 from github import Github
+import os
 
-def watchrepo(repo_name, GITHUB_TOKEN, watched):
+#take user token and name of repo
+
+def watch_repo(GITHUB_TOKEN, repo_name): #call: g.getrepo(repo_name)
     g = Github(GITHUB_TOKEN)
-    user = g.get_user()
     repo = g.get_repo(repo_name)
-    watch = user.add_to_watched(watched)
-    return watch
-
+    user = g.get_user() #login=NotSet by default
+    #check if user starred repo already
+    if user.has_in_watched(repo): #check if true
+        user.remove_from_watched(repo)
+    else:
+        user.add_to_watched(repo)
