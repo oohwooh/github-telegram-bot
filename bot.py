d384db5d23bce0508f5b3bdf71796c772ae21172
elizabethqiu <elizabethjqiu@gmail.com>
2020-08-02 18:03:37 -0400

    make minor edits

--- bot.py
+++ bot.py
@@ -1,62 +1,76 @@
-#Elizabeth is trying to use the tutorial
-#run: $Env:BOT_TOKEN = 'TOKEN_HERE'; python bot.py
+# Elizabeth is trying to use the tutorial
+# run: $Env:BOT_TOKEN = 'TOKEN_HERE'; python bot.py
 
 import os
 import github
 from github_functions import search_repos
-#some packages are built-in but for others you need to import // others req installing pip (pip install python-telegram-bot)
+# some packages are built-in but for others you need to import
+# others req installing pip (pip install python-telegram-bot)
 from telegram.ext import Updater
-from telegram.ext import PicklePersistence, CallbackQueryHandler # PicklePersistence: utility class, lets us save data about bot to a file
-from github_functions import repo_overview, star_repo, watch_repo # Need to import the function: in __init__.py, it imports the function from the file
+from telegram.ext import PicklePersistence, \
+    CallbackQueryHandler  # PicklePersistence: utility class, lets us save data about bot to a file
+from github_functions import repo_overview, star_repo, \
+    watch_repo  # Need to import the function: in __init__.py, it imports the function from the file
 
 persistence = PicklePersistence(filename='data')
-updater = Updater(token=os.getenv("BOT_TOKEN"), persistence = persistence, use_context=True) #use_context=True is special for v12 of library; default=False
-dispatcher = updater.dispatcher #introduce locally for updater quicker access to dispatcher
+updater = Updater(token=os.getenv("BOT_TOKEN"), persistence=persistence,
+                  use_context=True)  # use_context=True is special for v12 of library; default=False
+dispatcher = updater.dispatcher  # introduce locally for updater quicker access to dispatcher
 
-import logging #logging module setup to know when/why if things don't work
+import logging  # logging module setup to know when/why if things don't work
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
-#/start command calls this: returns welcome message
+# /start command calls this: returns welcome message
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello. I'm a bot, please talk to me! Type /help for more info.")
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Hello. I'm a bot, please talk to me! Type /help for more info.")
+
 
 def ping(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="pong✌😝✌")
+
 
 def help(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="""I will echo back all your statements while I'm running.
 Special commands include /start, /help, /caps [x], /ping, /auth [x], /repo [x]""")
 
+
 from telegram.ext import CommandHandler
+
 start_handler = CommandHandler('start', start)
 ping_handler = CommandHandler('ping', ping)
 help_handler = CommandHandler('help', help)
 dispatcher.add_handler(start_handler)
 updater.start_polling()
 
-#listens for regular messages and echoes them; MessageHandler class and Handler subclass
+
+# listens for regular messages and echoes them; MessageHandler class and Handler subclass
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 
 from telegram.ext import MessageHandler, Filters
+
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
 
 
-#/caps command calls this; takes text and replies in caps
-#can receive the arguments (as a list, split on spaces) passed to command in callback function
+# /caps command calls this; takes text and replies in caps
+# can receive the arguments (as a list, split on spaces) passed to command in callback function
 def caps(update, context):
     text_caps = ' '.join(context.args).upper()
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
+
 caps_handler = CommandHandler('caps', caps)
 dispatcher.add_handler(caps_handler)
 
-
-#/setinline command calls this; bot call on command via inline mode
+# /setinline command calls this; bot call on command via inline mode
 from telegram import InlineQueryResultArticle, InputTextMessageContent
+
+
 def inline_caps(update, context):
     query = update.inline_query.query
     if not query:
@@ -65,12 +79,12 @@ def inline_caps(update, context):
 
     results = [
         InlineQueryResultArticle(
-            id = repo.id,
-            title = repo.full_name,
-            input_message_content = InputTextMessageContent(repo_overview(repo, context.user_data['token'])[0]),
-            reply_markup = InlineKeyboardMarkup(repo_overview(repo, context.user_data['token'])[1]),
-            url = repo.html_url,
-            description = repo.description
+            id=repo.id,
+            title=repo.full_name,
+            input_message_content=InputTextMessageContent(repo_overview(repo, context.user_data['token'])[0]),
+            reply_markup=InlineKeyboardMarkup(repo_overview(repo, context.user_data['token'])[1]),
+            url=repo.html_url,
+            description=repo.description
         ) for repo in search_repos(context.user_data['token'], query)[:20]
     ]
     results.append(
@@ -84,6 +98,7 @@ def inline_caps(update, context):
 
 
 from telegram.ext import InlineQueryHandler
+
 inline_caps_handler = InlineQueryHandler(inline_caps)
 dispatcher.add_handler(inline_caps_handler)
 from telegram import InlineKeyboardMarkup
@@ -92,18 +107,20 @@ from telegram import InlineKeyboardMarkup
 def repo_info(update, context):
     if "token" in context.user_data:
         desc, keyboard = repo_overview(context.args[0], context.user_data["token"])
-        if keyboard: # If keyboard exists
+        if keyboard:  # If keyboard exists
             reply_markup = InlineKeyboardMarkup(keyboard)
         else:
             reply_markup = None
             context.bot.send_message(chat_id=update.effective_chat.id, text=desc, reply_markup=reply_markup)
     else:
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                text="Not logged in. Please use the /auth command to log in.")
+                                 text="Not logged in. Please use the /auth command to log in.")
 
 
 repo_info_handler = CommandHandler("repo", repo_info)
 dispatcher.add_handler(repo_info_handler)
+
+
 # use MessageHandler with command filter to reply to all unrecognized commands
 
 
@@ -113,48 +130,51 @@ def button(update, context):
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     # 'watch_xxxxx (repo id)'
     if query.data.startswith('star'):
-        repo_id = int(query.data.replace('star_', '')) # repo_id = 'xxxxxxx'
+        repo_id = int(query.data.replace('star_', ''))  # repo_id = 'xxxxxxx'
         # User's auth token will be update.user_data['token']
         string, repo_name = star_repo(context.user_data['token'], repo_id)
     if query.data.startswith('watch'):
-        repo_id = int(query.data.replace('watch_', '')) # repo_id = 'xxxxxxx'
+        repo_id = int(query.data.replace('watch_', ''))  # repo_id = 'xxxxxxx'
         # User's auth token will be update.user_data['token']
         string, repo_name = watch_repo(context.user_data['token'], repo_id)
     query.answer(text="The repo, " + repo_name + " has been " + string)
     # another way to do this is f"Selected option: {query.data}"
     # query.edit_message_text(text="Selected option: {}".format(query.data))
     # selected option: watch_277662457
+
+
 updater.dispatcher.add_handler(CallbackQueryHandler(button))
 
 
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
+
 from github import Github
+
 
 def auth(update, context):
     try:
         g = Github(context.args[0])
         user = g.get_user()
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Logged in as" + user.name + "(" + user.login + ")" )
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Logged in as " + user.name + "(" + user.login + ")")
         context.user_data["token"] = context.args[0]
-        #/auth will get set to user.context_data['token']
+        # /auth will get set to user.context_data['token']
 
     except:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Invalid input. Enter token like '/auth token'")
 
 
-auth_handler = CommandHandler("auth", auth )
+auth_handler = CommandHandler("auth", auth)
 dispatcher.add_handler(auth_handler)
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
 
-
-#added lastly: (if added it could be trigger before CommandHandlers has chance to update)
-#to circumvent this, keyword argument group (int) can be passed to add_handler with a value other than 0
-#updater.stop()
+# added lastly: (if added it could be trigger before CommandHandlers has chance to update)
+# to circumvent this, keyword argument group (int) can be passed to add_handler with a value other than 0
+# updater.stop()
 updater.idle()
-
 
 # results = list()
 # for repo in search_repos(query):
--- github_functions/repo_overview.py
+++ github_functions/repo_overview.py
@@ -2,20 +2,24 @@ from github import Github
 from telegram import InlineKeyboardButton
 from github import Repository
 
+
 def repo_overview(repo_name, GITHUB_TOKEN):
     if type(repo_name) == str:
         g = Github(GITHUB_TOKEN)
         try:
             repo = g.get_repo(repo_name)
         except:
-            return "repo name does not exist", None # Null object in Python
+            return "repo name does not exist", None  # Null object in Python
     elif type(repo_name) == Repository.Repository:
         repo = repo_name
-    if (repo.description): # Python will convert this: it means "if this repo description exists (true)"
+    if (repo.description):  # Python will convert this: it means "if this repo description exists (true)"
         desc = "Description: " + repo.description
     else:
         desc = ""
     languages = "\n Languages: " + str(repo.get_languages())
-    desc = desc + languages + "\n Watchers: " + str(repo.watchers_count) + " \n Stargazers: " + str(repo.stargazers_count) + " \n Forks: " + str(repo.forks_count)
-    keyboard = [[InlineKeyboardButton("⭐", callback_data='star_'+str(repo.id)), InlineKeyboardButton("👀", callback_data='watch_'+str(repo.id))], [InlineKeyboardButton("Open in GitHub", url=repo.url)]]
-    return desc, keyboard # desc in String, keyboard is nested list
+    desc = desc + languages + "\n Watchers: " + str(repo.watchers_count) + " \n Stargazers: " + str(
+        repo.stargazers_count) + " \n Forks: " + str(repo.forks_count)
+    keyboard = [[InlineKeyboardButton("⭐", callback_data='star_' + str(repo.id)),
+                 InlineKeyboardButton("👀", callback_data='watch_' + str(repo.id))],
+                [InlineKeyboardButton("Open in GitHub", url=repo.html_url)]]
+    return desc, keyboard  # desc in String, keyboard is nested list
